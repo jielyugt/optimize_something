475d0376f635777a3daee666fc9d08a21f925927
jielyugt <jie.lyu@gatech.edu>
2019-09-08 16:13:13 -0400

    optimization implemented

--- optimization.py
+++ optimization.py
@@ -30,7 +30,10 @@ import pandas as pd
 import matplotlib.pyplot as plt  		   	  			  	 		  		  		    	 		 		   		 		  
 import numpy as np  		   	  			  	 		  		  		    	 		 		   		 		  
 import datetime as dt  		   	  			  	 		  		  		    	 		 		   		 		  
-from util import get_data, plot_data  		   	  			  	 		  		  		    	 		 		   		 		  
+from util import get_data, plot_data  
+
+# additional import
+from scipy.optimize import minimize
   		   	  			  	 		  		  		    	 		 		   		 		  
 # This is the function that will be tested by the autograder  		   	  			  	 		  		  		    	 		 		   		 		  
 # The student must update this code to properly implement the functionality  		   	  			  	 		  		  		    	 		 		   		 		  
@@ -44,12 +47,15 @@ def optimize_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), \
     prices_SPY = prices_all['SPY']  # only SPY, for comparison later  
 
     # find the allocations for the optimal portfolio 
-
-    # note that the values here ARE NOT meant to be correct for a test case  
-    # for debugging purpose		   	  			  	 		  		  		    	 		 		   		 		  
-    #allocs = np.asarray([0.3, 0.1, 0.1, 0.3, 0.2])
-    allocs = np.asarray([0, 0, 1, 0])	    	 		 		   		 		  
-    cr, adr, sddr, sr = get_cr_adr_sddr_sr(allocs, prices)    
+    n = len(syms)
+    get_sr = lambda allocs: -get_cr_adr_sddr_sr(allocs, prices)[3]
+    init_allocs = [1/n] * n
+    cons = [{ "type": "eq", "fun": lambda allocs: sum(allocs) - 1}]
+    bnds = [(0,1) for _ in range(n)]
+    result = minimize(get_sr, init_allocs, method = "SLSQP", constraints = cons, bounds = bnds)
+    optimal_allocs = result.x
+    
+    cr, adr, sddr, sr = get_cr_adr_sddr_sr(optimal_allocs, prices)    
 
 
     # Get daily portfolio value  		   	  			  	 		  		  		    	 		 		   		 		  
@@ -61,7 +67,7 @@ def optimize_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), \
         df_temp = pd.concat([port_val, prices_SPY], keys=['Portfolio', 'SPY'], axis=1)  		   	  			  	 		  		  		    	 		 		   		 		  
         pass  		   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
-    return allocs, cr, adr, sddr, sr  		   	  			  	 		  		  		    	 		 		   		 		  
+    return optimal_allocs, cr, adr, sddr, sr  		   	  			  	 		  		  		    	 		 		   		 		  
 
 # returns the cumulative return, average daily return and volatility from a portfolio
 def get_cr_adr_sddr_sr(allocs, prices):
@@ -116,14 +122,11 @@ def test_code():
     end_date = dt.datetime(2010,1,1)  		   	  			  	 		  		  		    	 		 		   		 		  
     symbols = ['GOOG', 'AAPL', 'GLD', 'XOM', 'IBM']  		   	  			  	 		  		  		    	 		 		   		 		  
 
-    """
+    
     # Assess the portfolio  		   	  			  	 		  		  		    	 		 		   		 		  
     allocations, cr, adr, sddr, sr = optimize_portfolio(sd = start_date, ed = end_date,\
         syms = symbols, \
         gen_plot = False) 
-    """ 	
-
-    allocations, cr, adr, sddr, sr = optimize_portfolio() 	   	  			  	 		  		  		    	 		 		   		 		  
   		   	  			  	 		  		  		    	 		 		   		 		  
     # Print statistics  		   	  			  	 		  		  		    	 		 		   		 		  
     print(f"Start Date: {start_date}")  		   	  			  	 		  		  		    	 		 		   		 		  
